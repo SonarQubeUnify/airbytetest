efebca9772710d1dde7f150b390774ac0dfbf625
harshunify <harshavardhan.unify@gmail.com>
2022-10-18 10:36:00 +0530

    Initial

---
+++ airbyte-integrations/connectors/destination-s3/src/main/java/io/airbyte/integrations/destination/s3/S3Destination.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.destination.s3;
+
+import io.airbyte.integrations.base.IntegrationRunner;
+
+public class S3Destination extends BaseS3Destination {
+
+  public S3Destination() {}
+
+  public S3Destination(final S3DestinationConfigFactory s3DestinationConfigFactory) {
+    super(s3DestinationConfigFactory);
+  }
+
+  public static void main(String[] args) throws Exception {
+    new IntegrationRunner(new S3Destination()).run(args);
+  }
+
+  @Override
+  public StorageProvider storageProvider() {
+    return StorageProvider.AWS_S3;
+  }
+
+}
